7033ed53f8074f07224be9c01111adc64b54c059
Connor Adams <connorads@users.noreply.github.com>
2025-05-25 16:12:16 +0100

    Allow test to take arguments

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+import sys
 from agent import BasicAgent
 
 agent = BasicAgent()
 
-result = agent("What is the capital of France?")
+# Get the question from command-line arguments
+if len(sys.argv) > 1:
+    question = sys.argv[1]
+else:
+    question = "What is the capital of France?"
+
+
+result = agent(question)
 
 print(result)
