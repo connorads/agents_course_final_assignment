4cc3ef048e94c2abe0ba85568ccca08e95298c0c
Connor Adams <connorads@users.noreply.github.com>
2025-05-28 19:47:10 +0100

    Use Gemini

--- agent.py
+++ agent.py
@@ -1,15 +1,43 @@
+import os
 import logfire
 
 from pydantic_ai import Agent
-from tools import safe_duckduckgo_search_tool, get_youtube_transcript
+from google import genai
+from google.genai import types
+
 logfire.configure()  
 logfire.instrument_pydantic_ai()
+
+def web_search_tool(question: str) -> str | None:
+    """Given a question only, search the web to answer the question.
+
+        Args:
+            question (str): Question to answer
+            
+        Returns:
+            str: Answer to the question
+            
+        Raises:
+            RuntimeError: If processing fails"""
+    try:
+        client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-preview-05-20",
+            contents=question,
+            config=types.GenerateContentConfig(
+                tools=[types.Tool(google_search=types.GoogleSearch())]
+            )
+        )
+
+        return response.text
+    except Exception as e:
+        raise RuntimeError(f"Processing failed: {str(e)}") from e
 
 class BasicAgent:
     def __init__(self):
         self.agent = Agent(
-            "openai:o3-mini",
-            tools=[safe_duckduckgo_search_tool(), get_youtube_transcript],
+            "gemini-2.5-flash-preview-05-20",
+            tools=[web_search_tool],
             system_prompt="You are a helpful assistant that can answer questions about the world.",
         )
 
